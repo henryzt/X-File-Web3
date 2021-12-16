7814d885bbf4ccfd4728576dbb977b9422d3c5b9
Henry Zhang <henryzhang9802@gmail.com>
2021-12-16 11:26:39 +0800

    Add tailwind, change login logic

--- src/App.vue
+++ src/App.vue
@@ -7,16 +7,15 @@
 </template>
 
 <script lang="ts">
-/* eslint-disable */
 import { Options, Vue } from "vue-class-component";
 import Web3 from "web3";
-// @ts-expect-error
+// @ts-expect-error typing
 import contract from "truffle-contract";
-import ensTitle from '../build/contracts/EnsTitle.json';
+import ensTitle from "../build/contracts/EnsTitle.json";
 
 @Options({})
 export default class App extends Vue {
-  provider: any  = {};
+  provider: any = {};
   web3: any = {};
   account = {};
   titleApp: any = {};
@@ -24,43 +23,41 @@ export default class App extends Vue {
   async initWeb3Account() {
     if (window.ethereum) {
       this.provider = window.ethereum;
-      try {
-        await window.ethereum.enable();
-      } catch (error) {
-          console.log("User denied account access");
-      }
     } else if (window.web3) {
       console.log("User denied account access");
     } else {
       this.provider = new Web3.providers.HttpProvider("http://127.0.0.1:8545");
     }
     this.web3 = new Web3(this.provider);
-    console.log('henryDebug this.provider', this.provider);
-    console.log('henryDebug this.web3', this.web3);
-    this.web3.eth.getAccounts().then((accs: any) => {
-      console.log('henryDebug accs', accs);
-      this.account = accs[0];
-    });
+    console.log("henryDebug this.provider", this.provider);
+    console.log("henryDebug this.web3", this.web3);
   }
 
   async initContract() {
-    const titleContract = contract(ensTitle)
-    titleContract.setProvider(this.provider)
-    this.titleApp = await titleContract.deployed()
-    console.log("contract", this.titleApp)
-    const res = await this.titleApp.getTitles("formatz.eth");
-    console.log(res)
+    const titleContract = contract(ensTitle);
+    titleContract.setProvider(this.provider);
+    this.titleApp = await titleContract.deployed();
+    console.log("contract", this.titleApp);
+    this.$store.commit("setApp", this.titleApp);
   }
 
-  async getCrowdInfo() {}
+  async login() {
+    try {
+      await window.ethereum.enable();
+      this.web3.eth.getAccounts().then((accs: any) => {
+        console.log("henryDebug accs", accs);
+        this.account = accs[0];
+      });
+    } catch (error) {
+      console.log("User denied account access");
+    }
+  }
 
   async created(): Promise<void> {
     //  初始化 web3及账号
     await this.initWeb3Account();
     //  初始化合约实例
     await this.initContract();
-    //  获取合约的状态信息
-    await this.getCrowdInfo();
   }
 }
 </script>
--- src/store/index.ts
+++ src/store/index.ts
@@ -2,8 +2,12 @@ import { createStore } from 'vuex';
 
 export default createStore({
   state: {
+    solApp: null
   },
   mutations: {
+    setApp(state, app) {
+      state.solApp = app
+    }
   },
   actions: {
   },
